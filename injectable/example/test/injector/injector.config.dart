68ccc10facd18b4e7e17970e17319304aa09178a
DanhDue ExOICTIF <danhdue@gmail.com>
2022-12-05 13:46:09 +0700

    add configurations.

---
+++ injectable/example/test/injector/injector.config.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i1;
+
+/// ignore_for_file: unnecessary_lambdas
+/// ignore_for_file: lines_longer_than_80_chars
+extension GetItInjectableX on _i1.GetIt {}
